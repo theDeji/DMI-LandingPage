132882544180fec536eb7ce2dc6af207cfdd5d8e
theDeji <emmi2110@gmail.com>
2021-03-16 23:00:42 +0100

    new commmit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -34,7 +34,7 @@ const Footer = () => {
                 <FaYoutube />
               </SocialIconLink>
               <SocialIconLink
-                href="//www.twitter.com/briandesignz"
+                href="//www.twitter.com/_thedeji"
                 target="_blank"
                 aria-label="Twitter"
                 rel="noopener noreferrer"
