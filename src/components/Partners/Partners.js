cf96c4b86a8797363dad89a6b6092c07fe813f16
theDeji <emmi2110@gmail.com>
2021-03-16 23:30:15 +0100

    new commmit

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -1,42 +1,32 @@
-import React from 'react'
-import gs from '../../assets/gs.webp'
-import coke from '../../assets/coke.webp'
-import dmi from '../../assets/dmi.png'
-import mit from '../../assets/mit.webp'
-import cam from '../../assets/cam.svg'
-import oxford from '../../assets/oxford.jpg'
-import nestle from '../../assets/nestle.webp'
+import React from "react";
+import gs from "../../assets/gs.webp";
+import coke from "../../assets/coke.webp";
+import dmi from "../../assets/dmi.png";
+import mit from "../../assets/mit.webp";
+import cam from "../../assets/cam.svg";
+import oxford from "../../assets/oxford.jpg";
+import nestle from "../../assets/nestle.webp";
 
-
-import {
-    PartnerContent,
-    PartnerContainer
-} from './PartnerElements'
-import Icon from './Icon'
+import { PartnerContent, PartnerContainer } from "./PartnerElements";
+import Icon from "./Icon";
 
 const Partners = () => {
-    return(
-        <PartnerContent>
-            <h2>Our Parters</h2>
-            <PartnerContainer>
-                <div>
-                    <Icon image={cam} name="Lead Partner"/>
-                    <Icon image={mit} name="Official License"/>
-                    <Icon image={oxford} name="Official University"/>
-                    <Icon image={coke} name="Official Drink"/>
-                    <Icon image={dmi} name="Official Digital Marketer"/>
-                    <Icon image={nestle} name="Official Coffee"/>
-                    <Icon image={gs} name="Official Bank"/>
-                </div>
-            </PartnerContainer>
-        </PartnerContent>
-        
-    )
-}
+  return (
+    <PartnerContent>
+      <h2>Our Partners</h2>
+      <PartnerContainer>
+        <div>
+          <Icon image={cam} name="Lead Partner" />
+          <Icon image={mit} name="Official License" />
+          <Icon image={oxford} name="Official University" />
+          <Icon image={coke} name="Official Drink" />
+          <Icon image={dmi} name="Official Digital Marketer" />
+          <Icon image={nestle} name="Official Coffee" />
+          <Icon image={gs} name="Official Bank" />
+        </div>
+      </PartnerContainer>
+    </PartnerContent>
+  );
+};
 
-export default Partners
-
-// cambridge uni
-// mastercard
-// bank of america
-// Mit
+export default Partners;
