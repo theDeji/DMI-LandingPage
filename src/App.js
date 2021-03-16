c8dec1464c010233c556e939c2a1f28c8f1e7e46
theDeji <emmi2110@gmail.com>
2021-03-16 22:59:50 +0100

    new commmit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import {BrowserRouter as Router} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
+import Alumni from "./components/Alumni";
+import Feature from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+import Partners from "./components/Partners/Partners";
+import Support from "./components/Support/Support";
 
-import Alumni from './components/Alumni';
-import Feature from './components/Features';
-import Hero from './components/Hero';
-import Nav from './components/Nav';
-import Partners from './components/Partners/Partners';
-import Support from './components/Support/Support';
-
-
-import GlobalStyle from './globalStyles'
+import GlobalStyle from "./globalStyles";
 
 function App() {
   return (
     <Router>
-        <GlobalStyle />
-        <Nav />
-        <Hero/>
-        <Feature />
-        <Alumni />
-        <Partners />
-        <Support />
+      <GlobalStyle />
+      <Nav />
+      <Hero />
+      <Feature />
+      <Alumni />
+      <Partners />
+      <Support />
+      <Footer />
     </Router>
   );
 }
