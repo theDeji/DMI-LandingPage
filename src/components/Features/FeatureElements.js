0ad1159066c46fe5d3d1082cbf9d251cfac58040
theDeji <emmi2110@gmail.com>
2021-03-16 23:06:07 +0100

    new commmit

--- src/components/Features/FeatureElements.js
+++ src/components/Features/FeatureElements.js
@@ -1,75 +1,70 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const FContainer = styled.div`
   background: #ff4d4d;
-  padding-top:20px;
-  h2{
-    text-align:center;
-    color:white;
-    margin-bottom:30px;
+  padding-top: 20px;
+  h2 {
+    text-align: center;
+    color: white;
+    margin-bottom: 30px;
   }
 
   @media screen and (max-width: 1120px) {
     display: flex;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 
-  
   @media screen and (max-width: 540px) {
     display: flex;
-    flex-direction:column;
+    flex-direction: column;
   }
 `;
-
 
 export const FPackages = styled.div`
   height: 400px;
   display: flex;
-  flex-direction:row;
-  justify-content:center;
+  flex-direction: row;
+  justify-content: center;
   align-item: center;
-  padding: 0 30px; 
-
+  padding: 0 30px;
 
   @media screen and (max-width: 1120px) {
     height: 100%;
     display: flex;
     flex-direction: column;
-    justify-content:center;
-    align-items:center;
+    justify-content: center;
+    align-items: center;
   }
-  
+
   @media screen and (max-width: 540px) {
     height: 100%;
     display: flex;
     flex-direction: column;
-    justify-content:center;
-    align-items:center;
+    justify-content: center;
+    align-items: center;
   }
-
 `;
 
 export const FeatureBtn = styled.div`
-    width: 120px;
-    padding: 5px;
-    height: 40px;
-    background-color:#b33636;
-    border-radius: 10px;
-    margin-top: 20px;
-    color:white;
-    text-align:center;
-    transition: all .5s ease;
+  width: 120px;
+  padding: 5px;
+  height: 40px;
+  background-color: #b33636;
+  border-radius: 10px;
+  margin-top: 20px;
+  color: white;
+  text-align: center;
+  transition: all 0.5s ease;
 
-    
-    &:hover{
-      background-color:white;
-      transform: translateX(5px);
-        color: #ff4d4d;
-        cursor: pointer;
-    }
-`
+  &:hover {
+    background-color: white;
+    transform: translateX(5px);
+    color: #ff4d4d;
+    cursor: pointer;
+  }
+`;
 
 export const Packages = styled.div`
   display: flex;
@@ -88,11 +83,10 @@ export const Packages = styled.div`
     justify-content: center;
     align-items: center;
   }
-
-`
+`;
 
 export const ImageContainer = styled.div`
-  img{
+  img {
     width: 250px;
     height: 300px;
     border-radius: 10px;
@@ -100,14 +94,14 @@ export const ImageContainer = styled.div`
   }
 
   @media screen and (max-width: 1120px) {
-    img{
+    img {
       width: 200px;
       height: 200px;
     }
   }
 
   @media screen and (max-width: 540px) {
-    img{
+    img {
       width: 150px;
       height: 150px;
       border-radius: 10px;
@@ -115,47 +109,48 @@ export const ImageContainer = styled.div`
       margin: 40px 30px;
     }
   }
-`
-export const PackDesc =  styled.div`
+`;
+export const PackDesc = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   width: 50%;
   padding: 20px;
-  margin-left:10px;
-  line-height:30px;
+  margin-left: 10px;
+  line-height: 30px;
 
-  h3{
-    color:black;
-    margin-bottom:10px;
-    opacity: .7;
+  h3 {
+    color: black;
+    margin-bottom: 10px;
+    opacity: 0.7;
   }
 
-  h4{
+  h4 {
+    font-weight: bold;
     font-size: 13px;
-    color:white;
-    opacity: .9;
-    font-weight: light ;
+    color: white;
+    opacity: 0.9;
+    font-weight: light;
   }
-  
+
   @media screen and (max-width: 540px) {
     width: 80%;
     padding: 5px;
-    margin-left:10px;
+    margin-left: 10px;
     margin-bottom: 30px;
 
-    h3{
-      text-align:center;
-      color:black;
-      margin-bottom:10px;
-      opacity: .7;
+    h3 {
+      text-align: center;
+      color: black;
+      margin-bottom: 10px;
+      opacity: 0.7;
     }
 
-    h4{
+    h4 {
       font-size: 13px;
-      color:white;
-      opacity: .9;
-      font-weight: lighter ;
+      color: white;
+      opacity: 0.9;
+      font-weight: lighter;
     }
   }
-`
+`;
--- src/components/Point.js
+++ src/components/Point.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import { AiFillBulb } from 'react-icons/ai'
+import React from "react";
+import { AiFillBulb } from "react-icons/ai";
 
-
-export default function Point({title}) {
-    return (
-        <div style={{display:'flex', flexDirection:'row', justifyContent:'start', alignItems:'center',}}>
-            <AiFillBulb size={20} color='white'/>
-            <h4 style={{marginLeft:'10px'}}>{title}</h4>
-        </div>
-    )
+export default function Point({ title }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "start",
+        alignItems: "center",
+      }}
+    >
+      <AiFillBulb size={20} color="white" />
+      <h4 style={{ marginLeft: "10px", fontWeight: "bold" }}>{title}</h4>
+    </div>
+  );
 }
